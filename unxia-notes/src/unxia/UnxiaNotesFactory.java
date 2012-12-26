03e9f1a16db169221ae699315551630582ee6163
Marcus Warm <warm@mwvb.de>
2012-12-26 14:47:41 +0100

    Installationshinweis

--- unxia-notes/src/unxia/UnxiaNotesFactory.java
+++ unxia-notes/src/unxia/UnxiaNotesFactory.java
@@ -2,6 +2,20 @@ package unxia;
 
 /**
  * Unxia Factory für Lotus Notes 8.5
+ * 
+ * <h2>Wichtige Hinweise zur Eclipse Workspace Einrichtung:</h2>
+ * 
+ * <h3>NOTESJAR</h3>
+ * <p>Die Variable NOTESJAR muss auf die Notes.jar verweisen.
+ * z.B. 'C:\Program files (x86)\IBM\Lotus\Notes\jvm\ext\Notes.jar'
+ * <br>unxia-notes -> Rechtsklick -> Build Path -> Confige Build Path
+ * -> Libraries tab -> Add Variable button -> Configure Variables -> New
+ * -> NOTESJAR als Name eingeben und über File Button die Notes.jar auswählen.</p>
+ * 
+ * <h3>JRE</h3>
+ * <p>Es muss eine JRE namens "jvm" im Eclipse eingerichtet sein.
+ * Diese wird vom Projekt unxia-notes verwendet.
+ * Die JRE liegt z.B. im Ordner 'C:\Program files (x86)\IBM\Lotus\Notes\jvm'.</p>
  */
 public final class UnxiaNotesFactory implements UnxiaFactory {
 
